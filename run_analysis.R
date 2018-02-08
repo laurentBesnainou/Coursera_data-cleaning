17f34f4ffb4123b41a44f853c49e51fcb8db12fe
laurent Besnainou <laurent.besnainou@weave.eu>
2018-02-08 15:54:52 +0100

    update join instead of merge

--- run_analysis.R
+++ run_analysis.R
@@ -2,7 +2,8 @@
 library(dplyr)
 library(tidyr)
 
-
+############################################ Loading the files
+##############################################################
 #loading Activity and features tables 
 activities <- read.table('activity_labels.txt')
 features <- read.table('features.txt')
@@ -11,46 +12,47 @@ features <- read.table('features.txt')
 loadData <- function (rep){
   filex <- paste0(rep,"/X_",rep,".txt")
   filey <- paste0(rep,"/y_",rep,".txt")
+  filesubject <- paste0(rep,"/subject_",rep,".txt")
   x <- read.table(filex)
   y <- read.table(filey)
-  yLabels <- merge(y,activities,by.x="V1",by.y="V1")
+  subject <- read.table(filesubject)
+  names(subject) <- "subject"
+  yLabels <- left_join(y, activities, by = c("V1" = "V1"))
   #put colomns names
   names(x) <- features$V2
   names(yLabels) <- c("activity","activityname")
-  trainData <- cbind(yLabels,x)
+  cbind(subject,yLabels,x)
 }
+
 
 #Loading Training Data
 testData <- loadData("test")
 trainData<- loadData("train")
-#adding the type of data
-testData$type <- "test"
-trainData$type <- "train"
+
+#################################### Merge Test and Train Data
+##############################################################
 #merge the data
 mergeData <- rbind(testData,trainData)
 
-#get only mean and std columns
+
+############################### Getting all mean, std columns
+############################### plus subject and activityname
+##############################################################
 nameCol <- names(mergeData)
-elementMean <- grep("mean\\(\\)-[XYZ]$",nameCol)
-elementStd <- grep("std\\(\\)-[XYZ]$",nameCol)
-elementActivityType <- grep("activityname|type",nameCol)
-lstColmuns <- c(elementActivityType,elementMean,elementStd)
+lstColmuns <- grepl("subject|activityname|mean\\(\\)|std\\(\\)", nameCol)
 df <- mergeData[,lstColmuns]
-write.csv(df,"tidyDataframe.csv")
 
-#add the subject to dataframe
-subjecttrain <- read.table('train/subject_train.txt')
-subjecttest <- read.table('test/subject_test.txt')
-subject <- rbind(subjecttest,subjecttrain)
-names(subject) <- "subject"
-dfSubject <- cbind(subject,df)
-dfSubject$subject <- factor(dfSubject$subject )
-dfSubject$type <- NULL
-#Gather the dataframe to calculate the mean on each columns
-dfgather <- dfSubject %>% gather(variable, value, -subject, -activityname)
-dfAvg <- dfgather %>% group_by(activityname,subject,variable) %>% summarise(average=mean(value,na.rm = TRUE))
 
-#spreading the result to store in a new dataframe
-dfSpread <- dfAvg %>% spread(variable, average)
-write.csv(dfSpread,"AVGDataframe.csv")
+##################### Calculate mean of each colum groupy by 
+##################### group by activity name and subject
+##############################################################
+df$subject <- factor(df$subject )
+
+dfAvg <- df %>% group_by(activityname,subject) %>% 
+  summarise_all(funs(mean))
+
+##################### export the data as the result  
+##############################################################
+write.table(dfSpread,"AVGDataframe.txt",row.name=FALSE)
+
 
